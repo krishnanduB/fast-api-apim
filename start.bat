69682983157798d9bd274259bb148b1bc1aa1116
Krishnandu Baidya <krishkurosaki99@gmail.com>
2026-06-29 22:00:41 +0530

    Added endpoints, scripts, and README

--- start.bat
+++ start.bat
@@ -1,5 +1,7 @@
 @echo off
 SET PORT=8000
+echo Installing dependencies...
+pip install -r requirements.txt
 echo Starting FastAPI application on port %PORT%...
 start "FastAPI_Backend" cmd /k "uvicorn main:app --port %PORT% --reload"
 echo Application started in a new window.
